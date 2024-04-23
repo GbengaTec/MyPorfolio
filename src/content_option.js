3b6f6523671c7aa6fdfad32795986fb5f634dee0
unknown <gbengaTec>
2024-04-23 12:46:57 +0100

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,156 +1,132 @@
-const logotext = "JOHN";
+const logotext = "Gbenga";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Gbenga Busayo",
+    description: "I’m Gbenga Busayo software developer _ Front end devloper,currently working in South Africa",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I am Gbenga Busayo",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I Love To Code All Day Long and I Felt I Cant Live Without It A DAy",
+    your_img_url: "https://scontent.flos5-2.fna.fbcdn.net/v/t39.30808-6/353666846_278087774696696_7706680119603827508_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHFoOkPmnO2fUlxrRXZBAEgdoScp3aZfql2hJyndpl-qdL0SplA3yNnpnRldecoffGJ6W4nqHVV28ZYA9namtBD&_nc_ohc=fpl65Xlom9MAb7CTaQ_&_nc_zt=23&_nc_ht=scontent.flos5-2.fna&oh=00_AfDbjj9urYYjLzlamYQmFHIHSGoGI8R7VUqtulH7Z0y19w&oe=662D68B4",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "I am a skilled web developer proficient in React.js, HTML, and CSS, showcasing my projects with a focus on creating dynamic and interactive web solutions. Passionate about staying updated with industry trends and best practices.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "grocery bud",
+        where: "Ilorin",
+        date: "2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "shopright menu",
+        where: "Lagos",
+        date: "2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "countdown app",
+        where: "Ilorin",
+        date: "2023",
     },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
+const skills = [
     {
         name: "Javascript",
-        value: 80,
+        value: 70
     },
     {
         name: "React",
-        value: 60,
+        value: 70
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "HTML",
+        value: 90
+    },
+    {
+        name: "Bootsrap",
+        value: 70
+    },
+    {
+        name: "Sass",
+        value: 70
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "web developer",
+        description: "I design and develop responsive web applications, ensuring functionality, scalability, and user experience. My expertise includes problem-solving, code optimization, and staying updated with industry trends to deliver high-quality software solutions.",
     },
     {
         title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: " I focus on creating seamless user experiences, optimizing performance, and implementing features that enhance functionality and usability.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQGDMFLxZeFtuQ/feedshare-shrink_800/0/1713612872258?e=1717027200&v=beta&t=WjMbQtCeUElc8LD0Rf4aVs7wBU_ggYqdcSRLGBERY_k",
+        description: "this is my current payment app i am currently working with, i want to propose it to a company in US",
+        link: " https://gbengatec.github.io/React-Submenu/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQG5xw82_tJypQ/feedshare-shrink_2048_1536/0/1713587609162?e=1717027200&v=beta&t=vGQBhPQh2h2WsG3GfAet1xV-Nr9A3Z6eKcT9RChotcc",
+        description: "this is a grocery bud project i and a team of software freelance developer worked on for a grocery store called Grace store in Ilorin, ",
+        link: " https://gbengatec.github.io/Grocery-React-Project/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQGEu8zWSs4gnA/feedshare-shrink_2048_1536/0/1713587609849?e=1717027200&v=beta&t=HUK92W2cTpOVG6X0QecI7-Iiu9QshCJPfepxbR6VvGQ",
+        description: "menu item for shops and stores",
+        link: " https://gbengatec.github.io/Menu-React-App/",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQHWglSPxIlaCA/feedshare-shrink_800/0/1713603253598?e=1717027200&v=beta&t=0vej6-wQgxjH5wfVfR4huCLqvoNkStku08QNiuCQ4Pk",
+        description: "color generator that flip to typed color by convert hext-to-rgb",
+        link: " https://gbengatec.github.io/Color-Generator-React/",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQFsE7BgLiknUQ/feedshare-shrink_2048_1536/0/1707761025628?e=1717027200&v=beta&t=q-SxOgjjSNdOQH83sdbI9vZnT9vXgg561VL4e_UZcbk",
+        description: "countdown timer which i proses to esteem-gee supermarket in nigeria",
+        link: " https://gbengatec.github.io/JS-countdown/",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQGN2QM_zWdKNw/feedshare-shrink_800/0/1707765238615?e=1717027200&v=beta&t=kx58QEfGeMNxUe0uUbOpxGWLBmsQUct8ISsyA6umSM8",
+        description: "drum machine i build in FFC",
+        link: "https://codepen.io/dedicatedgbenga/pen/QWYgedG",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://media.licdn.com/dms/image/D4D22AQE0TrT3dzNJUg/feedshare-shrink_1280/0/1707765238474?e=1717027200&v=beta&t=KCXhFAUmax03Jj4jrrI9mqry51hhOUJADedMgYIaYu8",
+        description: "Random Quote Machine I Built on FFC",
+        link: "https://codepen.io/dedicatedgbenga/pen/oNmWxNL",
     },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+     {
+        img: "https://media.licdn.com/dms/image/D4D22AQGPdJ8VEK9ZMg/feedshare-shrink_1280/0/1707761417803?e=1717027200&v=beta&t=uKRAm69nOhv0fCarLUgOKKMXi-pAAg8I4VLZ4GNYYyI",
+        description: "Calculator machine i build in FFC",
+        link: "https://codepen.io/dedicatedgbenga/pen/WNPojGb",
     },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_EMAIL: "gbengabusayo765@gmail.com",
+    YOUR_FONE: "08105309866",
+    description: "i will be always available for your service, feel free to contact me ",
+    YOUR_SERVICE_ID: "service_ixr61gi",
+    YOUR_TEMPLATE_ID: "template_btjac1a",
+    YOUR_USER_ID: "gyswApZ8GMldEMG_F",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/GbengaTec",
+    facebook: "https://web.facebook.com/gbenga.ologbonsaiye.5",
+    linkedin: "https://www.linkedin.com/in/gbenga-busayo/",
+    twitter: "https://twitter.com/GbengaBusayo6",
 };
 export {
     meta,
